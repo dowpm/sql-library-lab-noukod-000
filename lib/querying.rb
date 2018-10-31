b4dd8ccb6f055e690f9578eb6f2de96066e44cdd
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-31 07:32:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT a.name, sub.name FROM authors AS a
+  JOIN series AS s ON a.id = s.author_id
+  JOIN subgenres AS sub ON s.subgenre_id = sub.id"
 end
 
 def select_series_title_with_most_human_characters
