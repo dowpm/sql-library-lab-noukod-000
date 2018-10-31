aee77ef28c2ba5019aaf29e3e6cf853e2a12a801
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-31 07:37:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT s.title FROM series AS s
   JOIN characters AS c ON s.id = c.series_id
-  GROUP BY s.title HAVING MAX(COUNT(c.id))"
+  GROUP BY s.title "
 end
 
 def select_character_names_and_number_of_books_they_are_in
